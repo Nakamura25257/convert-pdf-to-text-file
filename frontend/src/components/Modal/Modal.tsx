5e1b6828afe592cc86930a11ca86a0586d9b5eb6
Nakamura25257 <k.eagle160@gmail.com>
2025-08-13 16:32:18 +0900

    update

--- frontend/src/components/Modal/Modal.tsx
+++ frontend/src/components/Modal/Modal.tsx
@@ -2,12 +2,14 @@ import useModal from '../../hooks/useModal';
 import './style.css';
 
 interface ModalProps {
+  modalText: string;
+  fileName: string | undefined;
   onConfirm: (() => void);
   onCancel: (() => void);
 }
 
 export const Modal: React.FC<ModalProps> = (props) => {
-  const { onCancel, onConfirm } = props;
+  const { modalText, fileName, onCancel, onConfirm } = props;
 
   const handleModal = (e: React.MouseEvent<HTMLDivElement>) => {
     const ele = e.target as HTMLDivElement;
@@ -18,7 +20,8 @@ export const Modal: React.FC<ModalProps> = (props) => {
   return (
     <div className='modalWrapper' onClick={handleModal}>
       <div className='modalContainer'>
-        <h3 className='title'>PDFをテキストへ変換してもよろしいですか？</h3>
+        <h3 className='title' dangerouslySetInnerHTML={{__html: modalText}}></h3>
+        <p className='fileName'>{fileName}</p>
         <div className='btnContainer modal-btns'>
           <button className='btn cancel-btn' onClick={onCancel}>Cancel</button>
           <button className='btn upload-btn' onClick={onConfirm}>OK</button>
--- frontend/src/pages/Upload/UploadImage.tsx
+++ frontend/src/pages/Upload/UploadImage.tsx
@@ -1,19 +1,16 @@
-import { useState } from 'react';
+import { ChangeEvent, ChangeEventHandler, useState } from 'react';
 import { Header } from '../../components/Header/Header';
 import { Modal } from '../../components/Modal/Modal';
 import useAuthForm from '../../hooks/useAuthForm';
 import './style.css';
 import useModal from '../../hooks/useModal';
 
-interface ModalProps {
-  open(): (() => void);
-  close(): (() => void);
-}
-
 /**
  * pdfアップロード画面
  */
 export const UploadImage: React.FC = () => {
+  const [modalText, setModalText] = useState<string>('');
+  const [file, setFile] = useState<File | null>(null);
   // モーダル開閉
   const { open, close, isModalOpen } = useModal();
 
@@ -22,29 +19,55 @@ export const UploadImage: React.FC = () => {
     console.log('logout');
   };
 
-  const handleConvertPdf = () => {
+  // PDFをテキストへ変換処理
+  const handleConvertPdf = async () => {
+    if(!file) {
+      return;
+    }
+    const formData = new FormData();
+    formData.append('file', file);
     // API fetch 処理
+    const url = 'http://localhost:5000/upload';
+    try {
+      const res = await fetch(url, {
+        method: 'POST',
+        body: formData,
+      });
+      console.log('res', res);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
+  const onChangeFile = (e: ChangeEvent<HTMLInputElement>): void => {
+    const file: File | null = e.target.files?.[0] || null;
+    if(file) {
+      setFile(file);
+      setModalText('以下のPDFをテキストへ変換します。<br> よろしいですか？')
+    }
+  }
 
   return (
     <div className='uploadMain'>
-      {isModalOpen && <Modal onConfirm={handleConvertPdf} onCancel={close}/>}
+      {isModalOpen && <Modal modalText={modalText} fileName={file?.name} onConfirm={handleConvertPdf} onCancel={close}/>}
       <Header title='Upload PDF' authText='Logout' onSwitch={handleLogout} />
       {/* would like to upload image by drag&drop */}
       <div className='uploadContainer'>
         <label className='fileLabel'>
-          ファイルを選択
+          {file?.name ? file.name : '選択してください'}
           <input
             type="file"
             className='fileInput'
-            onChange={(e) => console.log(e.target.files)}
+            onChange={onChangeFile}
+            // accept='.pdf'
           />
         </label>
       </div>
-
       <div className='btnContainer'>
-        <button className='btn upload-btn' onClick={open}>Upload</button>
+        <button 
+          className={file?.name ? 'btn upload-btn' : 'btn disabled'} 
+          onClick={open}
+          >Upload</button>
       </div>
     </div>
   )
